4946b7a0dd79111f72bf65d28c131677a68ace5f
oliverhuangcode <oliverwhuang@gmail.com>
2025-01-23 15:32:27 +1100

    Create basic structure of job details component

--- frontend/src/app/jobs/page.tsx
+++ frontend/src/app/jobs/page.tsx
@@ -4,6 +4,18 @@ import JobList from "@/components/jobs/details/job-list";
 import JobDetails from "@/components/jobs/details/job-details";
 import { Title } from "@mantine/core";
 
+const mockJobDetails = {
+  title: "Job Title",
+  company: "Company Name",
+  type: "Graduate",
+  locations: ["Location 1", "Location 2", "Location 3"],
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aeneantincidunt urna ac luctus pellentesque.",
+  studyFields: ["Field 1", "Field 2", "Field 3"],
+  posted: "01/01/2025",
+  workingRights: ["Right 1", "Right 2", "Right 3"],
+};
+
 export default function JobsPage() {
   return (
     <div className="space-y-4">
@@ -19,7 +31,7 @@ export default function JobsPage() {
         {/* Sticky Job Details - hidden on mobile, 70% on desktop */}
         <div className="hidden lg:block lg:w-[65%]">
           <div className="overflow-y-auto h-[calc(100vh-330px)]">
-            <JobDetails />
+            <JobDetails {...mockJobDetails} />
           </div>
         </div>
       </div>
--- frontend/src/components/jobs/details/job-details.tsx
+++ frontend/src/components/jobs/details/job-details.tsx
@@ -1,9 +1,108 @@
-import { Text } from "@mantine/core";
+import { Text, Title, Badge, Group, Button, Card, Stack, Avatar, Divider }  from "@mantine/core";
+import {IconMapPin} from '@tabler/icons-react';
 
-export default function JobDetails() {
+interface JobDetailsProps {
+  title: string;
+  company: string;
+  type: string;
+  locations: string[];
+  description: string;
+  studyFields: string[];
+  workingRights: string[];
+  posted: string;
+  logo?: string; 
+}
+
+export default function JobDetails({
+  title,
+  company,
+  type,
+  locations,
+  description,
+  studyFields,
+  posted,
+  logo,
+  workingRights,
+}: JobDetailsProps) {
   return (
-    <div className="bg-neutral-700 p-4 rounded-lg h-full">
-      <Text className="font-semibold mb-4 text-center">Details here</Text>
-    </div>
+    <Card shadow="sm" padding="lg" radius="md" withBorder className="h-full">
+      {/* Header Section */}
+      <Stack gap="sm" mb="md">
+        <Group justify="space-between" align="center" mb="md">
+          {/* Logo and Company Name */}
+          <Group align="center">
+            <Avatar src={logo} size="lg" radius="md" />
+            <Text fw={500}>{company}</Text>
+          </Group>
+
+          {/* Apply Now Button */}
+          <Button variant="filled" color="rgb(255, 226, 47)" radius="md">
+            <Text c="dark">Apply Now</Text>
+          </Button>
+        </Group>
+        
+        <Title order={2}>
+          {title}
+        </Title>
+        <Group>
+          <IconMapPin
+            size={20}
+            stroke={1.5}
+          />
+          <Group gap="xs" wrap="wrap">
+            {locations.map((location) => (
+              <Badge autoContrast key={location} color="dark.4" size="lg">
+                {location}
+              </Badge>
+            ))}
+          </Group>
+          <Divider color="rgb(255, 226, 47)" orientation="vertical"/>
+          <Text size="sm">
+            Posted {posted}
+          </Text>
+          <Divider color="rgb(255, 226, 47)" orientation="vertical"/>
+          <Text size="sm">
+            {type}
+          </Text>
+        </Group>
+        
+      </Stack>
+
+
+      {/* Description Section */}
+      <Stack gap="sm" mt="md" mb="lg">
+        <Title order={4}>Job Description</Title>
+        <Text size="sm" color="dimmed">
+          {description}
+        </Text>
+      </Stack>
+
+
+      {/* Study Field Section */}
+      <Stack gap="sm" mt="md" mb="md">
+        <Title order={4}>Study Fields</Title>
+        <Group gap="xs" wrap="wrap">
+          {studyFields.map((field) => (
+            <Badge autoContrast key={field} color="dark.4" size="lg">
+              {field}
+            </Badge>
+          ))}
+        </Group>
+      </Stack>
+      
+
+      {/* Working Rights Section */}
+      <Stack gap="sm" mt="md" mb="md">
+        <Title order={4}>Working Rights</Title>
+        <Group gap="xs" wrap="wrap">
+          {workingRights.map((rights) => (
+            <Badge autoContrast key={rights} color="dark.4" size="lg">
+              {rights}
+            </Badge>
+          ))}
+        </Group>
+      </Stack>
+      
+    </Card>
   );
 }
