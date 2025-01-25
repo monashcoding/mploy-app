8190e4c7cf3bdeaae25289b1c3b7d831723427ad
Kyran Menezes <kyranmenezesaus@gmail.com>
2025-01-25 19:43:14 +1100

    Ignored pnpm lock and fixed lint

--- frontend/src/lib/theme.ts
+++ frontend/src/lib/theme.ts
@@ -4,13 +4,13 @@ import tailwindConfig from "@/../tailwind.config";
 const twConfig = resolveConfig(tailwindConfig);
 
 const theme = createTheme({
-    colors:{
-        primary: colorsTuple(twConfig.theme.colors.primary),
-        text: colorsTuple(twConfig.theme.colors.text),
-        background: colorsTuple(twConfig.theme.colors.background),
-        secondary: colorsTuple(twConfig.theme.colors.secondary),
-        accent: colorsTuple(twConfig.theme.colors.accent)
-    }
-})
+  colors: {
+    primary: colorsTuple(twConfig.theme.colors.primary),
+    text: colorsTuple(twConfig.theme.colors.text),
+    background: colorsTuple(twConfig.theme.colors.background),
+    secondary: colorsTuple(twConfig.theme.colors.secondary),
+    accent: colorsTuple(twConfig.theme.colors.accent),
+  },
+});
 theme.primaryColor = "secondary";
-export {theme};
+export { theme };
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,22 +1,21 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'text': 'var(--text)',
-                'background': 'var(--background)',
-                'primary': 'var(--primary)',
-                'secondary': 'var(--secondary)',
-                'accent': 'var(--accent)',
-                
-            },
-        },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        text: "var(--text)",
+        background: "var(--background)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 } satisfies Config;
