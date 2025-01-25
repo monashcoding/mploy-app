9645a52da2a2920b61b47523f4bb415c7a4f17c6
oliverhuangcode <oliverwhuang@gmail.com>
2025-01-25 13:15:13 +1100

    Create basic home page setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { Button, Stack, Title, Center, Group } from "@mantine/core";
+
 export default function Page() {
-  return <div>Home Page</div>;
+  const handleGradJobsClick = () => {
+    // Navigate to the grad jobs page or perform a search action
+    console.log("Search Grad Jobs");
+  };
+
+  const handleInternJobsClick = () => {
+    // Navigate to the intern jobs page or perform a search action
+    console.log("Search Intern Jobs");
+  };
+  return (
+    <Center style={{ height: "100vh", flexDirection: "column" }}>
+      <Title order={1} mb="lg">
+        Discover the role you've been dreaming of
+      </Title>
+      <Group gap="md" align="center">
+        <Button autoContrast size="lg" variant="filled" color="#FFE22F" onClick={handleGradJobsClick}>
+          Search Grad Jobs
+        </Button>
+        <Button autoContrast size="lg" variant="filled" color="#FFE22F" onClick={handleInternJobsClick}>
+          Search Intern Jobs
+        </Button>
+      </Group>
+    </Center>
+  );
 }
