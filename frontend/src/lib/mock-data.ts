c52a8e6e2a6b3199b9daa58146bb99fc5afb9c88
Ed <edwinli123456@gmail.com>
2025-01-27 02:17:21 +1100

    delete mock data

--- frontend/src/lib/mock-data.ts
+++
@@ -1,9 +0,0 @@
-// frontend/src/lib/mock-data.ts
-import { Job } from "@/types/job";
-import jobsData from "./jobs.json";
-import { transformJobData } from "./transform-job-data";
-
-// Honestly i don't know what this does and why is it here
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-export const MOCK_JOBS: Job[] = (jobsData as never[]).map(transformJobData);
