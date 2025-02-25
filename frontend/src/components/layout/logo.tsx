72a47dd66ad952b41b03476d14d1987444defa09
JasonA <49816459+jason301c@users.noreply.github.com>
2025-02-25 11:51:28 +1100

    Make Logo a server component by using <Link> and adding suspense

--- frontend/src/components/layout/logo.tsx
+++ frontend/src/components/layout/logo.tsx
@@ -1,13 +1,21 @@
 import MacLogo from "@/assets/mac.svg";
 import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
+
 export default function Logo() {
   return (
-    <div
-      className="flex space-x-3 items-center h-8 cursor-pointer"
-      onClick={() => (window.location.href = "/")}
-    >
-      <Image src={MacLogo} className={"h-12 w-12"} alt="MAC Logo" />
+    <Link href="/" className="flex space-x-3 items-center h-8 cursor-pointer">
+      <div className="h-12 w-12 relative">
+        <Suspense
+          fallback={
+            <div className="h-12 w-12 bg-gray-600 rounded-md animate-pulse" />
+          }
+        >
+          <Image src={MacLogo} className={"h-12 w-12"} alt="MAC Logo" />
+        </Suspense>
+      </div>
       <span className="text-xl">Jobs</span>
-    </div>
+    </Link>
   );
 }
