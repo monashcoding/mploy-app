b780bdec2bc490ee62af01319296dffbd1a4f2a0
ed <edwinli123456@gmail.com>
2025-02-27 16:25:06 +1100

    remove extra divs

--- frontend/src/app/jobs/page.tsx
+++ frontend/src/app/jobs/page.tsx
@@ -25,30 +25,26 @@ export default async function JobsPage({
   const { jobs, total } = await getJobs(await searchParams);
 
   return (
-    <div className="">
+    <>
       <FilterSection _totalJobs={total} />
 
       {total <= 0 ? (
         <NoResults />
       ) : (
         <div className="mt-4 flex flex-col lg:flex-row">
-          <div className="w-full lg:w-[35%]">
-            <div id="job-list-container" className="lg:pr-1">
-              <Suspense fallback={<JobListLoading />}>
-                <JobList jobs={jobs} />
-              </Suspense>
-            </div>
+          <div id="job-list-container" className="lg:pr-1 w-full lg:w-[35%]">
+            <Suspense fallback={<JobListLoading />}>
+              <JobList jobs={jobs} />
+            </Suspense>
           </div>
 
-          <div className="hidden lg:block lg:w-[65%]">
-            <div className="overflow-y-auto h-[calc(100vh-200px)]">
-              <Suspense fallback={<JobDetailsLoading />}>
-                <JobDetails />
-              </Suspense>
-            </div>
+          <div className="hidden lg:block lg:w-[65%] overflow-y-auto h-[calc(100vh-200px)]">
+            <Suspense fallback={<JobDetailsLoading />}>
+              <JobDetails />
+            </Suspense>
           </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
