f8b4b7f706808d0399e9242c3d55174b36c63a1a
oliverhuangcode <oliverwhuang@gmail.com>
2025-03-01 18:49:21 +1100

    Adjust height calc

--- frontend/src/app/jobs/[id]/page.tsx
+++ frontend/src/app/jobs/[id]/page.tsx
@@ -27,7 +27,7 @@ export default async function JobDetailPage({ params }: PageProps) {
       <div className="flex items-center justify-center m0 p0">
       <div
           className="rounded-lg overflow-y-auto"
-          style={{ height: "calc(100vh - 160px)",width: "110%", maxWidth: "800px" }}
+          style={{ height: "calc(100vh - 120px)",width: "110%", maxWidth: "800px" }}
         >
           <JobDetailsWrapper job={job} />
         </div>
