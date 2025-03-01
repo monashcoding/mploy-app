5c198c2adda3276f2c3bbd687df0c544a8be6454
Ed <edwinli123456@gmail.com>
2025-03-02 05:48:02 +1100

    replace logo with real svg

--- frontend/src/components/layout/logo.tsx
+++ frontend/src/components/layout/logo.tsx
@@ -5,13 +5,7 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/" className="flex space-x-3 items-center h-8 cursor-pointer">
-      <Image
-        src={MacLogo}
-        className="h-10 w-10"
-        alt="MAC Logo"
-        placeholder="blur"
-        blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8cEf3PwAHtwL6YluwEgAAAABJRU5ErkJggg=="
-      />
+      <Image src={MacLogo} className="h-10 w-10" alt="MAC Logo" />
       <span className="text-lg lg:text-xl">Jobs</span>
     </Link>
   );
