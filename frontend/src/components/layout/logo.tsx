fa947c178cb09fa2df0e2d61a93073b077dd1f2f
jason301c <jasonalex.st@gmail.com>
2025-09-25 16:49:56 +1000

    Implement caching properly for both dev and prod workflows and dockerfiles

--- frontend/src/components/layout/logo.tsx
+++ frontend/src/components/layout/logo.tsx
@@ -1,11 +1,10 @@
-import MacLogo from "@/assets/mac.svg";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Logo() {
   return (
     <Link href="/" className="flex space-x-3 items-center h-8 cursor-pointer">
-      <Image src={MacLogo} className="h-10 w-10" alt="MAC Logo" />
+      <Image src="/mac.svg" className="h-10 w-10" alt="MAC Logo" />
       <span className="text-lg lg:text-xl">Jobs</span>
     </Link>
   );
