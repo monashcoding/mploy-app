901b139c5a30e1e1a53ed407d76eb8a485ed9774
jason301c <jasonalex.st@gmail.com>
2025-09-25 17:01:32 +1000

    Fix MAC logo taking up whole screen

--- frontend/src/components/layout/logo.tsx
+++ frontend/src/components/layout/logo.tsx
@@ -4,7 +4,13 @@ import Link from "next/link";
 export default function Logo() {
   return (
     <Link href="/" className="flex space-x-3 items-center h-8 cursor-pointer">
-      <Image src="/mac.svg" className="h-10 w-10" alt="MAC Logo" />
+      <Image
+        src="/mac.svg"
+        width={40}
+        height={40}
+        className="h-10 w-10"
+        alt="MAC Logo"
+      />
       <span className="text-lg lg:text-xl">Jobs</span>
     </Link>
   );
