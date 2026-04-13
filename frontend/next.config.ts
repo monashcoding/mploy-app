f4542adf1646337ee9d52faab480058524dc133d
steven <spha0034@student.monash.edu>
2026-04-13 18:57:12 +1000

    fix: mogno pino and pino-pretty webpack

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  serverExternalPackages: ["mongodb", "pino", "pino-pretty"],
 };
 
 export default nextConfig;
