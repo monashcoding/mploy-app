8d6787164cebc1e9d931862ed25dba3c759a382c
Jason Yi <jasonyi1413@gmail.com>
2026-07-26 17:22:54 +1000

    feat(frontend): display min mascot for 404 page

--- frontend/src/app/jobs/[id]/not-found.tsx
+++ frontend/src/app/jobs/[id]/not-found.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function JobNotFound() {
   return (
     <div className="flex min-h-[80vh] flex-col items-center justify-center gap-6 px-4 text-center">
-      <Image src="/mac.svg" alt="MAC mascot" width={160} height={160} />
+      <Image src="/min-404.svg" alt="404 mascot" width={200} height={200} />
       <h1 className="text-4xl font-bold text-white">404</h1>
       <p className="text-lg text-white/50">
         This job post is no longer taking applications or it does not exist.
